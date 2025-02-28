1ddbce63ca41f34a1244d8d996adc5f983d89d66
Weyero <48177328+Weyero@users.noreply.github.com>
2025-03-01 00:51:52 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import { App } from './App'
+
+function Overlay() {
+  return (
+    <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>ok —</div>
+      <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>22/12/2022</div>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root');
+rootElement.innerHTML = ''; // Очищаем старый контент
+
+const root = createRoot(rootElement);
+root.render(
+  <>
+    <App />
+    <Overlay />
+  </>
+);
